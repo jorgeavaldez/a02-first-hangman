a184f79ac00531d483c5549b0c6abb70584c37d8
Jorge Valdez <jorge@realgeeks.com>
2018-09-13 14:26:16 -0500

    added module subdir and api delegates; updated test for empty new_game

---
+++ hangman/lib/hangman/game.ex
@@ -0,0 +1,13 @@
+defmodule Hangman.Game do
+  def new_game do
+    :empty
+  end
+
+  def tally(game) do
+    game
+  end
+
+  def make_move(game, guess) do
+    guess
+  end
+end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -2,7 +2,7 @@ defmodule HangmanTest do
   use ExUnit.Case
   doctest Hangman
 
-  test "greets the world" do
-    assert Hangman.hello() == :world
+  test "contains new_game" do
+    assert Hangman.new_game() == :empty
   end
 end
